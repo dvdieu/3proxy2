445d80486cfd51ee7ddc1313ed8dcd27df0314ef
Your Name <you@example.com>
2023-08-01 01:00:08 +0700

    123

--- ipv6-with-port-none-password.sh
+++ ipv6-with-port-none-password.sh
@@ -45,7 +45,6 @@ deny *
 $(awk -F "/" '{print "auth none\n" \
 "" $1 "\n" \
 "allow * 124.158.10.223 \n" \
-
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "socks -6 -n -a -p" $4+10000 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
