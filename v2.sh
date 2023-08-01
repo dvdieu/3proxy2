4ea138e6d407fe434cc6a7336509def8f937412e
Your Name <you@example.com>
2023-08-02 01:02:44 +0700

    123

--- v2.sh
+++ v2.sh
@@ -21,9 +21,7 @@ install_3proxy() {
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp bin/3proxy /usr/local/etc/3proxy/bin/
-    cp ../init.d/3proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
+
     cd $WORKDIR
     #cp src/3proxy /usr/local/etc/3proxy/bin/
 
